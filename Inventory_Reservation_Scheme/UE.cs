dc14a5a7e72be97033c8c681c1ca3e6a236c99c2
cagehuang <jackhuan.tw@yahoo.com.tw>
2015-05-25 17:11:26 +0800

    Add UE.cs

---
+++ Inventory_Reservation_Scheme/UE.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Reservation_Scheme
+{
+    class UE
+    {     
+        // UID
+        
+        // Type (regular or variable)
+        
+        // Cycle_period
+        
+        // Consumed_quota (per cycle)
+        
+        // GU
+        
+        // Signalings (Number of signalings) 
+
+
+    }
+}
