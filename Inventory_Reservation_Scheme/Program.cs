a32b98c4df040076a10359ca82060219578b4567
cagehuang <jackhuan.tw@yahoo.com.tw>
2015-05-25 15:22:22 +0800

    Initial Commit

---
+++ Inventory_Reservation_Scheme/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Reservation_Scheme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
